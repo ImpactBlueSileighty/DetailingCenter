8b17d4874f49cde1053bfb1168db860cd650b4a0
Igor Denisov <igor.denisov@esedulainen.fi>
2021-02-21 22:59:49 +0300

    Added Navigation Buttons, added some Styles, added some Data and new Attributes in DataBase

--- DetailingCenter/Windows/ServiceList.xaml.cs
+++ DetailingCenter/Windows/ServiceList.xaml.cs
@@ -21,10 +21,72 @@ namespace DetailingCenter
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
             //serviceList.ItemsSource = context.Service.ToList();
+            
+
+        }
+
+        private void clientsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(clientsBtn.IsChecked == true)
+            {
+                navigationRec1.Fill = Brushes.Orange;
+                navigationRec2.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec3.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec4.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                financeBtn.IsChecked = false;
+                serviceBtn.IsChecked = false;
+                ordersBtn.IsChecked = false;
+            }
+            
+        }
+
+        private void serviceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (serviceBtn.IsChecked == true)
+            {
+                navigationRec2.Fill = Brushes.Orange;
+                navigationRec1.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec3.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec4.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                clientsBtn.IsChecked = false;
+                financeBtn.IsChecked = false;
+                ordersBtn.IsChecked = false;
+            }
+        }
+
+        private void financeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(financeBtn.IsChecked == true)
+            {
+                navigationRec3.Fill = Brushes.Orange;
+                navigationRec1.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec2.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec4.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                clientsBtn.IsChecked = false;
+                serviceBtn.IsChecked = false;
+                ordersBtn.IsChecked = false;
+            }
+        }
+
+        
+
+        private void ordersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ordersBtn.IsChecked == true)
+            {
+                navigationRec4.Fill = Brushes.Orange;
+                navigationRec1.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec2.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec3.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                clientsBtn.IsChecked = false;
+                financeBtn.IsChecked = false;
+                serviceBtn.IsChecked = false;
+            }
         }
     }
 }
