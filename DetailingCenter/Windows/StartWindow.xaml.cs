9e17ff9ca5e5c8908fcc458750ad45bbc045592e
Igor Denisov <igor.denisov@esedulainen.fi>
2021-03-03 00:18:55 +0300

    Added HeaderButton event on click, fixed size of window, fixed db attributes

---
+++ DetailingCenter/Windows/StartWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static DetailingCenter.EF.AppData;
+using DetailingCenter.Pages;
+using DetailingCenter.Windows;
+
+namespace DetailingCenter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            //serviceList.ItemsSource = context.Service.ToList();
+            
+
+        }
+
+
+        private void windowSizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                this.ResizeMode = ResizeMode.CanResizeWithGrip;
+            }
+            else
+            {
+                this.ResizeMode = ResizeMode.NoResize;
+                this.WindowState = WindowState.Maximized;
+                
+            }
+            
+        }
+
+        private void windowCloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void windowTaskBarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void headerGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void calendarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (calendarBtn.IsChecked == true)
+            {
+                navigationRec1.Fill = Brushes.Orange;
+                navigationRec2.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec3.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec4.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+
+                clientsBtn.IsChecked = false;
+                financeBtn.IsChecked = false;
+                serviceBtn.IsChecked = false;
+                
+
+            }
+        }
+
+        private void clientsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClientPage cp = new ClientPage();
+            if (clientsBtn.IsChecked == true)
+            {
+                navigationRec1.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec2.Fill = Brushes.Orange;
+                navigationRec3.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec4.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+
+                calendarBtn.IsChecked = false;
+                financeBtn.IsChecked = false;
+                serviceBtn.IsChecked = false;
+                navigationFrame.Navigate(cp);
+                
+            }
+
+        }
+
+        private void serviceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ServicePage sp = new ServicePage();
+            if (serviceBtn.IsChecked == true)
+            {
+                navigationRec3.Fill = Brushes.Orange;
+                navigationRec1.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec2.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec4.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+
+                calendarBtn.IsChecked = false;
+                clientsBtn.IsChecked = false;
+                financeBtn.IsChecked = false;
+                
+
+                navigationFrame.Navigate(sp);
+            }
+        }
+
+        private void financeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (financeBtn.IsChecked == true)
+            {
+                navigationRec4.Fill = Brushes.Orange;
+                navigationRec1.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec2.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+                navigationRec3.Fill = new SolidColorBrush(Color.FromRgb(11, 8, 20));
+
+                calendarBtn.IsChecked = false;
+                clientsBtn.IsChecked = false;
+                serviceBtn.IsChecked = false;
+               
+            }
+        }
+
+
+
+        
+    }
+}
