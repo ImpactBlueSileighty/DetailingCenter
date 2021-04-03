243578bdf2eeff76d1697c905b2788551baa7962
Igor Denisov <igor.denisov@esedulainen.fi>
2021-04-03 11:42:46 +0300

    Revert "Revert "Added AddEditWindow and functional like add new order and edit it.""
    This reverts commit f2bef148c61c4456b63b1de71e2ad92d27a02939.

--- DetailingCenter/Model/EF/OrderStatus.cs
+++ DetailingCenter/Model/EF/OrderStatus.cs
@@ -10,13 +10,20 @@
 namespace DetailingCenter.Model.EF
 {
     using System;
+    using System.Collections.Generic;
     
-    public partial class sp_helpdiagrams_Result
+    public partial class OrderStatus
     {
-        public string Database { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public OrderStatus()
+        {
+            this.Order = new HashSet<Order>();
+        }
+    
+        public int Id { get; set; }
         public string Name { get; set; }
-        public int ID { get; set; }
-        public string Owner { get; set; }
-        public int OwnerID { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Order { get; set; }
     }
 }
---
+++ DetailingCenter/View/Windows/EditOrderWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DetailingCenter.Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DetailingCenter.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditOrder.xaml
+    /// </summary>
+    public partial class EditOrderWindow : Window
+    {
+        EditOrderWindow_VM VM;
+
+        public EditOrderWindow(Order SelectedOrder=null)
+        {
+            VM = new EditOrderWindow_VM(SelectedOrder);
+            DataContext = VM;
+            InitializeComponent();
+        }
+
+        private void windowTaskBarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void windowCloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void headerGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            VM.SaveChanges();
+            this.Close();
+        }
+    }
+}
